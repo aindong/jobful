59fc32902b6b79853e1006a24522d90442563b05
rachel <rachel@petixe.com>
2015-01-25 03:38:05 +0800

    requests

--- app/models/TrainingRequest.php
+++ app/models/TrainingRequest.php
@@ -1,7 +1,7 @@
 <?php
 
 class TrainingRequest extends \Eloquent {
-	protected $fillable = ['lname', 'fname', 'mname', 'baranggay', 'locality', 'district', 'contact1', 'contact2', 'email', 'participants'];
+	protected $fillable = ['lname', 'fname', 'mname', 'baranggay', 'locality', 'district', 'contact1', 'contact2', 'email', 'participants', 'status'];
     protected $table = 'requests';
     public static $rules = array(
         'fname' => 'required',
