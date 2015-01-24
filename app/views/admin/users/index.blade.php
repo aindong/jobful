b862221b142927d8369173da6841828b0ec012fd
Nina Kristel Cheng <nkcheng@koodi.ph>
2015-01-25 02:10:25 +0800

    users listing

---
+++ app/views/admin/users/index.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.admin')
+
+@section('content')
+
+    <table id="example" class="display table table-striped" cellspacing="0" width="100%">
+        <thead>
+        <tr>
+            <th></th>
+            <th>Email</th>
+            <th>Password</th>
+            <th>Created</th>
+            <th>Updated</th>
+        </tr>
+        </thead>
+
+        <tfoot>
+        <tr>
+            <th></th>
+            <th>Email</th>
+            <th>Password</th>
+            <th>Created</th>
+            <th>Updated</th>
+        </tr>
+        </tfoot>
+            @foreach($users as $user)
+                <tr>
+                    <td></td>
+                    <td>{{{ $user->email }}}</td>
+                    <td>{{{ $user->password }}}</td>
+                    <td>{{{ $user->created_at }}}</td>
+                    <td>{{{ $user->updated_at }}}</td>
+                </tr>
+            @endforeach
+        <tbody>
+
+        </tbody>
+    </table>
+@stop
+
+@section('page-script')
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('#example').DataTable();
+        });
+    </script>
+@stop
