337cf5cd9d6bfd4d1f836160a1a1ee76b62d4227
Nina Kristel Cheng <nkcheng@koodi.ph>
2015-01-24 18:36:54 +0800

    added listing

--- app/views/admin/employers/index.blade.php
+++ app/views/admin/employers/index.blade.php
@@ -5,15 +5,9 @@
         <thead>
         <tr>
             <th></th>
-            <th>Last Name</th>
-            <th>First Name</th>
-            <th>Middle Name</th>
-            <th>Address 1</th>
-            <th>Address 2</th>
-            <th>Locality</th>
-            <th>Administrative Level 1</th>
-            <th>Administrative Level 2</th>
-            <th>Country</th>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Status</th>
             <th>Created</th>
             <th>Updated</th>
             <th>Actions</th>
@@ -23,15 +17,9 @@
         <tfoot>
         <tr>
             <th></th>
-            <th>Last Name</th>
-            <th>First Name</th>
-            <th>Middle Name</th>
-            <th>Address 1</th>
-            <th>Address 2</th>
-            <th>Locality</th>
-            <th>Administrative Level 1</th>
-            <th>Administrative Level 2</th>
-            <th>Country</th>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Status</th>
             <th>Created</th>
             <th>Updated</th>
             <th>Actions</th>
@@ -40,15 +28,9 @@
             @foreach($employers as $employer)
                 <tr>
                     <td></td>
-                    <td>{{{ $employer->lname }}}</td>
-                    <td>{{{ $employer->fname }}}</td>
-                    <td>{{{ $employer->mname }}}</td>
-                    <td>{{{ $employer->address1 }}}</td>
-                    <td>{{{ $employer->address2 }}}</td>
-                    <td>{{{ $employer->locality }}}</td>
-                    <td>{{{ $employer->administrative_level1 }}}</td>
-                    <td>{{{ $employer->administrative_level2 }}}</td>
-                    <td>{{{ $employer->country }}}</td>
+                    <td>{{{ $employer->title }}}</td>
+                    <td>{{{ $employer->description }}}</td>
+                    <td>{{{ $employer->status }}}</td>
                     <td>{{{ $employer->created_at }}}</td>
                     <td>{{{ $employer->updated_at }}}</td>
                     <td>Add | Delete</td>
--- app/views/admin/trainers/index.blade.php
+++ app/views/admin/trainers/index.blade.php
@@ -1,1 +1,69 @@
-C:\Projects\jobful\app/views/trainers/index.blade.php
+@extends('layouts.admin')
+
+@section('content')
+    <table id="example" class="display table table-striped" cellspacing="0" width="100%">
+        <thead>
+        <tr>
+            <th></th>
+            <th>Last Name</th>
+            <th>First Name</th>
+            <th>Middle Name</th>
+            <th>Address 1</th>
+            <th>Address 2</th>
+            <th>Locality</th>
+            <th>Administrative Level 1</th>
+            <th>Administrative Level 2</th>
+            <th>Country</th>
+            <th>Created</th>
+            <th>Updated</th>
+            <th>Actions</th>
+        </tr>
+        </thead>
+
+        <tfoot>
+        <tr>
+            <th></th>
+            <th>Last Name</th>
+            <th>First Name</th>
+            <th>Middle Name</th>
+            <th>Address 1</th>
+            <th>Address 2</th>
+            <th>Locality</th>
+            <th>Administrative Level 1</th>
+            <th>Administrative Level 2</th>
+            <th>Country</th>
+            <th>Created</th>
+            <th>Updated</th>
+            <th>Actions</th>
+        </tr>
+        </tfoot>
+            @foreach($trainers as $trainer)
+                <tr>
+                    <td></td>
+                    <td>{{{ $trainer->lname }}}</td>
+                    <td>{{{ $trainer->fname }}}</td>
+                    <td>{{{ $trainer->mname }}}</td>
+                    <td>{{{ $trainer->address1 }}}</td>
+                    <td>{{{ $trainer->address2 }}}</td>
+                    <td>{{{ $trainer->locality }}}</td>
+                    <td>{{{ $trainer->administrative_level1 }}}</td>
+                    <td>{{{ $trainer->administrative_level2 }}}</td>
+                    <td>{{{ $trainer->country }}}</td>
+                    <td>{{{ $trainer->created_at }}}</td>
+                    <td>{{{ $trainer->updated_at }}}</td>
+                    <td>Add | Delete</td>
+                </tr>
+            @endforeach
+        <tbody>
+
+        </tbody>
+    </table>
+@stop
+
+@section('page-script')
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('#example').DataTable();
+        });
+    </script>
+@stop
