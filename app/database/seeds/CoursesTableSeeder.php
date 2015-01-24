581a7a4af1f8e0d55ee92c8f3f0df42a4323e27f
rachel <rachel@petixe.com>
2015-01-24 19:49:22 +0800

    add course seed

--- app/database/seeds/CoursesTableSeeder.php
+++ app/database/seeds/CoursesTableSeeder.php
@@ -7,14 +7,81 @@ class CoursesTableSeeder extends Seeder {
 
 	public function run()
 	{
-		$faker = Faker::create();
+		Course::create([
+            'code' => 'course',
+            'title' => 'Small Engine (Yanmar, Kubota)',
+            'description' => '',
+            'duration' => '10',
+            'duration_metric' => 'day',
+            'type' => 'skill',
+        ]);
 
-		foreach(range(1, 10) as $index)
-		{
-			Course::create([
-                
-			]);
-		}
+        Course::create([
+            'code' => 'course',
+            'title' => 'Cellphone Repair',
+            'description' => '',
+            'duration' => '10',
+            'duration_metric' => 'day',
+            'type' => 'skill',
+        ]);
+
+        Course::create([
+            'code' => 'course',
+            'title' => 'Dress Making / ISMO',
+            'description' => '',
+            'duration' => '10',
+            'duration_metric' => 'day',
+            'type' => 'skill',
+        ]);
+
+        Course::create([
+            'code' => 'course',
+            'title' => 'Housekeeping',
+            'description' => '',
+            'duration' => '10',
+            'duration_metric' => 'day',
+            'type' => 'skill',
+        ]);
+
+        Course::create([
+            'code' => 'course',
+            'title' => 'Catering',
+            'description' => '',
+            'duration' => '5',
+            'duration_metric' => 'day',
+            'type' => 'skill',
+        ]);
+
+        Course::create([
+            'code' => 'course',
+            'title' => 'Message Therapy',
+            'description' => '',
+            'duration' => '10',
+            'duration_metric' => 'day',
+            'type' => 'skill',
+        ]);
+
+        Course::create([
+            'code' => 'course',
+            'title' => 'Motorcycle Mechanics',
+            'description' => '',
+            'duration' => '10',
+            'duration_metric' => 'day',
+            'type' => 'skill',
+        ]);
+
+        Course::create([
+            'code' => 'course',
+            'title' => 'Shielded Metal Arc Wedding',
+            'description' => '',
+            'duration' => '10',
+            'duration_metric' => 'day',
+            'type' => 'skill',
+        ]);
+
+        
+        
+
 	}
 
 }
