dcb1e9808fc84812161bd458b07f835d0b733be6
Alleo Indong <freedom_war550@live.com.ph>
2015-01-24 17:33:31 +0800

    Created an example admin layout

---
+++ app/views/layouts/admin.blade.php
@@ -0,0 +1,14 @@
+<html>
+<header>
+
+    <link rel="stylesheet" href="/packages/DataTables-1.10.4/media/css/jquery.dataTables.min.css"/>
+    @yield('page-style')
+</header>
+<body>
+    @yield('section')
+
+    <script type="text/javascript" src="/js/jquery/jquery.min.js"></script>
+    <script type="text/javascript" src="/packages/DataTables-1.10.4/media/js/jquery.dataTables.min.js"></script>
+    @yield('page-script')
+</body>
+</html>
